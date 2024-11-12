53e5e4a427d6acd4e54f2ba65cdae0bee75c2785
Do-yoon <1.kim.doyoon@gmail.com>
2024-11-12 14:20:27 +0900

    Revert "[hotfix] CORS 설정"

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from api.interface.controllers.controller import router
 from containers import Container
@@ -11,20 +10,6 @@ app.include_router(router=router)
 
 llm_manager = LLMManager.get_instance()
 llm_manager.load_model()
-
-# CORS
-origins = [
-    "https://52.78.141.49:8080",
-    "http://localhost:8080",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 if __name__ == "__main__":
     uvicorn.run(
