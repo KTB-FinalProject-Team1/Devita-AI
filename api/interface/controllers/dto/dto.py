ccdd97f094fe34c7f552541b64969565e42656ec
Do-yoon <1.kim.doyoon@gmail.com>
2024-12-20 18:17:35 +0900

    DK-286 dto 수정

--- api/interface/controllers/dto/dto.py
+++ api/interface/controllers/dto/dto.py
@@ -27,7 +27,7 @@ class AutonomousResponseDTO(BaseModel):
 class CompletedMissionRequestDTO(BaseModel):
     userId: int
     title: str
-    date: str
+    date: date
     missionType: str
     missionCategory: str
 
